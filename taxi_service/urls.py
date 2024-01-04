7a6f5efbac7f99a70ff40c8c3424d07b09d12cb2
Olexandr <maxbvling23432@gmail.com>
2024-01-04 15:06:05 +0200

    Merge remote-tracking branch 'origin/dev' into dev

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path("", include("taxi.urls", namespace="taxi")),
+    #path("", include("taxi.urls", namespace="taxi")),
     path("taxi/", include("taxi.urls", namespace="taxi")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
